cace906f7a7faf6f8f96b56e4288f364680a3d6f
gusgonza <yowcloud@outlook.es>
2025-02-11 16:25:47 +0100

    ♻️ refactor: se ha movido el archivo de JwtTokenService.java a la carpeta Util

--- src/main/java/com/auth/security/controller/AuthController.java
+++ src/main/java/com/auth/security/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.auth.security.dto.AuthResponse;
 import com.auth.security.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping( "/auth" )
@@ -18,6 +15,11 @@ public class AuthController {
     @Autowired
     public AuthController( AuthService authService ) {
         this.authService = authService;
+    }
+
+    @GetMapping( "/hello" )
+    public ResponseEntity< String > getHello() {
+        return authService.getHello();
     }
 
     @PostMapping( "/login" )
