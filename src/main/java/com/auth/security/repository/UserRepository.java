b0b400303b91e608350fb346e5c7d40a561d32da
gusgonza <yowcloud@outlook.es>
2025-02-18 17:08:02 +0100

    📝 docs: documentacion de UserRepository

--- src/main/java/com/auth/security/repository/UserRepository.java
+++ src/main/java/com/auth/security/repository/UserRepository.java
@@ -3,8 +3,25 @@ package com.auth.security.repository;
 import com.auth.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+/**
+ * Repositorio para la entidad User.
+ * Proporciona métodos para realizar operaciones CRUD en la base de datos.
+ */
+public interface UserRepository extends JpaRepository< User, Long > {
 
+    /**
+     * Busca un usuario por su correo electrónico.
+     *
+     * @param email el correo electrónico del usuario
+     * @return el usuario encontrado o null si no se encuentra
+     */
+    User findByEmail( String email );
+
+    /**
+     * Busca un usuario por su nombre de usuario.
+     *
+     * @param username el nombre de usuario
+     * @return el usuario encontrado o null si no se encuentra
+     */
     User findByUsername( String username );
 }
