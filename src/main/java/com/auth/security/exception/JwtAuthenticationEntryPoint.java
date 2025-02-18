558930c03d28bb95f0406cf4cbe4fb7bc5e72a67
gusgonza <yowcloud@outlook.es>
2025-02-18 17:09:06 +0100

    📝 docs: documentacion de JwtAuthenticationEntryPoint

--- src/main/java/com/auth/security/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/auth/security/exception/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.auth.security.exception;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -9,9 +8,21 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+/**
+ * Punto de entrada de autenticación JWT.
+ * Maneja los intentos de acceso no autorizados enviando un error 401 (Unauthorized).
+ */
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    /**
+     * Método que se invoca cuando un usuario no autenticado intenta acceder a un recurso protegido.
+     *
+     * @param request       la solicitud HTTP
+     * @param response      la respuesta HTTP
+     * @param authException la excepción de autenticación
+     * @throws IOException sí ocurre un error de entrada/salida
+     */
     @Override
     public void commence( HttpServletRequest request , HttpServletResponse response , AuthenticationException authException )
             throws IOException {
