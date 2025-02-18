83f5d43eb401125cea460105d07e683933520f5d
gusgonza <yowcloud@outlook.es>
2025-02-18 17:09:24 +0100

    📝 docs: documentacion de AuthRequest

--- src/main/java/com/auth/security/dto/AuthResponse.java
+++ src/main/java/com/auth/security/dto/AuthResponse.java
@@ -4,16 +4,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Clase que representa la respuesta de autenticación.
+ */
 @Setter
 @Getter
 @NoArgsConstructor
-
 public class AuthResponse {
+
+    /**
+     * Token JWT generado tras la autenticación.
+     */
     private String token;
 
+    /**
+     * Constructor de la clase AuthResponse.
+     *
+     * @param token el token JWT generado tras la autenticación
+     */
     public AuthResponse( String token ) {
         this.token = token;
     }
-
-
 }
